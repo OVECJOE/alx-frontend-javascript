feaa69d6ede2aaddc9ec66989595b3a55af0c43b
OVECJOE <ovecjoe123@gmail.com>
2022-04-05 21:15:33 +0100

    Added:
    - README.md (forgot that initially);
    
    - Task 5 scripts

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,16 +3,12 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const output = [];
 
-  Promise.all([uploadPhoto, createUser])
+  Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
-      values[0]().then((result) => {
-        output.push(result.body);
-      });
-      values[1]().then((result) => {
-        for (const a of Object.keys(result)) {
-          output.push(result[a]);
-        }
-        console.log(output.join(' '));
-      });
+      output.push(values[0].body);
+      for (const key of Object.keys(values[1])) {
+        output.push(values[1][key]);
+      }
+      console.log(output.join(' '));
     }).catch('Signup system offline');
 }
