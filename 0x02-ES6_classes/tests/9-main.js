703d44a3c7d0984bfd689308e5bd8e4f2d9b2734
OVECJOE <ovecjoe123@gmail.com>
2022-04-12 12:18:24 +0100

    Modified the source code of Task 9; trying to fix bugs that caused output check fail

--- 0x02-ES6_classes/tests/9-main.js
+++ 0x02-ES6_classes/tests/9-main.js
@@ -1,9 +1,12 @@
-import listOfStudents from '../9-hoisting';
+import listOfStudents, { HolbertonClass, StudentHolberton } from '../9-hoisting';
+const assert = require('assert');
 
 console.log(listOfStudents);
 
 const listPrinted = listOfStudents.map(
-  student => student.fullStudentDescription,
+  (student) => student.fullStudentDescription,
 );
 
 console.log(listPrinted);
+console.log(listOfStudents[0].holbertonClass instanceof HolbertonClass);
+console.log(listOfStudents[0] instanceof StudentHolberton);
